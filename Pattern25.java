8d79f7354ffc7f188c2883c2b42d78086737e4fc
sivaravali <106945151+sivaravali@users.noreply.github.com>
2022-06-20 09:50:25 +0530

    Add files via upload

---
+++ Pattern25.java
@@ -0,0 +1,43 @@
+package patterns;
+
+/**
+ * 
+ * @author sv22044 1 10 11 20 21
+ *                 2 9 12 19 22
+ *                 3 8 13 18 23
+ *                 4 7 14 17 24
+ *                 5 6 15 16 25
+ *   
+ * 
+ */
+public class Pattern25 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		int x;
+		int y;
+		for (int i = 1; i <= n; i++) {
+			x = i;
+			y= n-i + 1;
+
+			for (int j = 1; j <= n; j++) {
+				if (j % 2 == 1) {
+					System.out.printf(" %2d", x);
+
+				} else {
+					System.out.printf(" %2d", y);
+				}
+				x = x + n;
+				y = y + n;
+
+			}
+			System.out.println();
+		}
+	}
+}
+
+
+
+
+
+
